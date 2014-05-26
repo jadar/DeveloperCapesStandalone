f0dcc7cdaff8c6173b8841250b5aa9b6a7d8394b
Jake Rhoda <jake@jadarstudios.com>
2014-05-26 10:16:31 -0500

    update DevCapesStandalone to be less confusing.

--- src/main/java/com/jadarstudios/developercapes/standalone/DevCapesStandalone.java
+++ src/main/java/com/jadarstudios/developercapes/standalone/DevCapesStandalone.java
@@ -1,69 +1,40 @@
 /**
  * DeveloperCapes by Jadar
  * License: MIT License (https://raw.github.com/jadar/DeveloperCapes/master/LICENSE)
- * version 2.1
+ * version 3.0
  */
 package com.jadarstudios.developercapes.standalone;
 
-import static argo.jdom.JsonNodeBuilders.aStringBuilder;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import argo.jdom.JdomParser;
-import argo.jdom.JsonRootNode;
-import argo.saj.InvalidSyntaxException;
-
-import com.google.common.base.Strings;
-import com.google.common.base.Throwables;
-import com.jadarstudios.developercapes.DevCapesUtil;
-
+import com.jadarstudios.developercapes.DevCapes;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.relauncher.Side;
 
-@Mod(name="DeveloperCapes", modid="devcapesstandalone", version="2.1")
+import java.io.InputStream;
+
+@Mod(name="DeveloperCapes", modid="devcapesstandalone", version="${version}")
 public class DevCapesStandalone {
 
 	@Instance
 	public static DevCapesStandalone instance;
-	
-	private static JdomParser parser = new JdomParser();
-	
-	private String capeTextUrl = "";
-	
-	/**
-	 * Loads information from jar JSON file.
-	 * 
-	 * @param event
-	 */
-	@EventHandler
-	public void preLoad(FMLPreInitializationEvent event) {
-		InputStream is = getClass().getResourceAsStream("/capeInfo.json");
-        InputStreamReader reader = new InputStreamReader(is);
-        try {
-            JsonRootNode root = parser.parse(reader);
 
-            capeTextUrl = Strings.nullToEmpty(root.getFields().get(aStringBuilder("capeTxtUrl")).getStringValue());
-
-        } catch (InvalidSyntaxException e) {
-            System.out.println("[DevCapes] Failed to parse capeInfo.json. This means you could have a corrupt mod jar.");
-        } catch (Exception e) {
-            throw Throwables.propagate(e);
-        }
-	}
-	
 	/**
 	 * Sets up DeveloperCapes.
-	 * 
+	 *
 	 * @param event
 	 */
 	@EventHandler
 	public void load(FMLInitializationEvent event) {
-		if(!Strings.isNullOrEmpty(capeTextUrl))
-			DevCapesUtil.addFileUrl(capeTextUrl);
+		if(FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT)
+        {
+            DevCapes.logger.info("Initializing DevCapes Standalone.");
+            InputStream stream = this.getClass().getResourceAsStream("/capeInfo.json");
+            if (stream == null) return;
+            DevCapes.getInstance().registerConfig(stream, "devcapesstandalone");
+        }
 	}
 	
 }
