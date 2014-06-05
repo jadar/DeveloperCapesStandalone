92a0969bef16402a6ca892f20cfabdf0655d1520
Jake Rhoda <jake@jadarstudios.com>
2014-06-04 22:25:21 -0400

    reverted back to old way of getting the URL from the web. It makes more sense so users can create a web app to automatically change the file remotely without changing the jar file.

--- src/main/java/com/jadarstudios/developercapes/standalone/DevCapesStandalone.java
+++ src/main/java/com/jadarstudios/developercapes/standalone/DevCapesStandalone.java
@@ -5,21 +5,62 @@
  */
 package com.jadarstudios.developercapes.standalone;
 
+import com.google.common.base.Strings;
+import com.google.common.base.Throwables;
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
 import com.jadarstudios.developercapes.DevCapes;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.relauncher.Side;
+import org.apache.logging.log4j.Logger;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 
-@Mod(name="DeveloperCapes", modid="devcapesstandalone", version="${version}")
+@Mod(name="DeveloperCapes", modid="DevCapesStandalone", version="${version}")
 public class DevCapesStandalone {
 
 	@Instance
 	public static DevCapesStandalone instance;
+    public static Logger logger;
+
+    public String capeConfigUrl;
+    public String modId;
+
+    @EventHandler
+    public void preLoad(FMLPreInitializationEvent event) {
+        this.logger = event.getModLog();
+        this.modId = event.getModMetadata().modId;
+
+        InputStream is = getClass().getResourceAsStream("/capeInfo.json");
+        InputStreamReader reader = new InputStreamReader(is);
+        try {
+            Gson gson = new Gson();
+
+            HashMap map = gson.fromJson(reader, HashMap.class);
+
+            Object urlStringObject = map.get("capeConfigUrl");
+            if (urlStringObject != null && (urlStringObject instanceof String)) {
+                capeConfigUrl = Strings.nullToEmpty((String)urlStringObject);
+            } else {
+                throw new JsonParseException("Could not find key 'capeConfigUrl'");
+            }
+        } catch (JsonParseException e) {
+            logger.error("Failed to parse capeInfo.json. This means you could have a corrupt mod jar.");
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+
+    }
+
+
 
 	/**
 	 * Sets up DeveloperCapes.
@@ -30,10 +71,9 @@ public class DevCapesStandalone {
 	public void load(FMLInitializationEvent event) {
 		if(FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT)
         {
-            DevCapes.logger.info("Initializing DevCapes Standalone.");
-            InputStream stream = this.getClass().getResourceAsStream("/capeInfo.json");
-            if (stream == null) return;
-            DevCapes.getInstance().registerConfig(stream, "devcapesstandalone");
+            logger.info("Initializing DevCapes Standalone.");
+            if (this.capeConfigUrl == null) return;
+            DevCapes.getInstance().registerConfig(this.capeConfigUrl, this.modId);
         }
 	}
 	
